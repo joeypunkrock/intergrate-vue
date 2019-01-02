0ac413ef79a1bf4ed71b1e3dcd4914de5d53268f
Joey Thomas <joeypunkrock11@gmail.com>
2019-01-02 12:22:44 +0000

    added some more test components

--- app.js
+++ app.js
@@ -3,22 +3,46 @@ Vue.component('todo-item', {
     template: '<li>{{ todo.text }}</li>'
 });
 
-Vue.component('range-item', {
+Vue.component('range-slider', {
     props: ['range']
 });
 
-var app = new Vue({
-    el: '#app',
+var todoItem = new Vue({
+    el: '#todo_item',
     data: {
         groceryList: [
             { id: 0, text: 'Vegetables' },
             { id: 1, text: 'Cheese' },
             { id: 2, text: 'Milk' }
         ],
+    }
+});
+
+var rangeSlider = new Vue({
+    el: '#range_slider',
+    data: {
         rangeList: [
             { id: 0, value: '10' },
             { id: 1, value: '50' },
             { id: 2, value: '85' }
         ]
+    },
+    methods: {
+        updateValue (e) {
+            this.value = e.target.value;
+            console.log(this.value);
+        },
     }
 });
+
+// Define a new component called button-counter
+Vue.component('button-counter', {
+    data: function () {
+      return {
+        count: 0
+      }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+});
+
+new Vue({ el: '#components-demo' });
